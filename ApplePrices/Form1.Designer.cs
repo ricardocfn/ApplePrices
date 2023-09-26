976497dfffc738c5f524eff997d49be43ae74c55
Ricardo C <ricardocamposfn@gmail.com>
2023-09-26 07:21:39 -0300

    Add project files.

---
+++ ApplePrices/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ApplePrices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(402, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(517, 488);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox2.Font = new System.Drawing.Font("Inter Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(135, 436);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(244, 32);
+            this.comboBox2.TabIndex = 13;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.Font = new System.Drawing.Font("Inter Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "iPhone",
+            "Macbook",
+            "Watch",
+            "iPad"});
+            this.comboBox1.Location = new System.Drawing.Point(135, 341);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(244, 32);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Inter", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 436);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 28);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Inter", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 341);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 28);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "iDevice";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Inter", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 114);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(513, 28);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tabela de preços - Lançamentos Apple 2023";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(163, 84);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Inter", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(445, 683);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 28);
+            this.label4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Inter", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(532, 683);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 28);
+            this.label5.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ApplePrices/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ApplePrices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            comboBox2.Text = string.Empty; // INICIAR O COMBOBOX SEM QUALQUER SELEÇÃO
+
+            if (comboBox1.SelectedIndex == 0) // APLICAR MODELO COM BASE NA SELEÇÃO
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("iphone15");
+                comboBox2.Items.Add("iphone15pro");
+                comboBox2.Items.Add("iphone15promax");
+
+            }
+            else if (comboBox1.SelectedIndex == 1) //ESCALAR  ADICIONANDO NOVOS ITENS
+            {
+                comboBox2.Items.Clear();
+                label4.Text = string.Empty;
+                label5.Text = string.Empty;
+                pictureBox2.Image = null;
+            }
+            else if (comboBox1.SelectedIndex == 2) //ESCALAR  ADICIONANDO NOVOS ITENS
+            {
+                comboBox2.Items.Clear();
+                label4.Text = string.Empty;
+                label5.Text = string.Empty;
+                pictureBox2.Image = null;
+            }
+            else if (comboBox1.SelectedIndex == 3) //ESCALAR  ADICIONANDO NOVOS ITENS
+            {
+                comboBox2.Items.Clear();
+                label4.Text = string.Empty;
+                label5.Text = string.Empty;
+                pictureBox2.Image = null;
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string[] Prices =
+              {
+                    "A partir de R$ 7.299",
+                    "A partir de R$ 9.299",
+                    "A partir de R$ 10.999"
+                };
+
+            string[] devices = Directory.GetFiles(@"C:\Users\daila\OneDrive\Área de Trabalho\C#\img"); // A STRING SERÁ O CAMINHO DO ARQUIVO POR EX: C:/USER/PROGRAM/PHOTO.JPG
+
+
+            foreach (string device in devices) // OBTER A ÚLTIMA PARTE DO NOME DO CAMINHO ARMAZENADO
+            {
+                string[] deviceName = device.Split('\\'); // SEPARA INDICES SEMPRE QUE HOUVER MA BARRA '/'
+
+                string[] deviceName2 = deviceName[7].Split('.'); // SEPARA ÍNDIES SEMPRE QUE HOUVER PONTO '.'
+
+
+
+
+                if (comboBox2.Text == deviceName2[0]) // DEFININDO A IMAGEM A SER MOSTRADA COM BASE NA SELEÇÃO COMBOBOX2
+                {
+                    pictureBox2.ImageLocation = "C:\\Users\\daila\\OneDrive\\Área de Trabalho\\C#\\img\\" + comboBox2.Text + ".png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    label4.Text = "Preço: ";
+
+                }
+                else if (comboBox2.SelectedIndex == 0) // APLICAR PREÇOS COM BASE NA SELEÇÃO
+                {
+                    label5.Text = Prices[0];
+                }
+                else if (comboBox2.SelectedIndex == 1) // APLICAR PREÇOS COM BASE NA SELEÇÃO
+                {
+                    label5.Text = Prices[1];
+                }
+                else if (comboBox2.SelectedIndex == 2) // APLICAR PREÇOS COM BASE NA SELEÇÃO
+                {
+                    label5.Text = Prices[2];
+                }
+
+            }
+
+        }
+    }
+}
